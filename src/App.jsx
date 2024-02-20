d0084c49654b6f7a4b7b8491b658592bc1769d15
Alme <josealejandrogilmendez08@gmail.com>
2024-02-19 21:21:45 -0400

    import and return upcomming component
    fetch upcomming games and pass the results to upcomming
    
    remove 'flex flex-col gap-12' of the principal div
    and add 'flex flex-col gap-14 py-14' to main tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Carousel from './components/Carousel';
 import Nav from './components/Nav';
 import CarouselPlaceholder from './components/CarouselPlaceholder';
 import Trending from './components/trending';
+import Upcomming from './components/Upcomming';
 
 function App() {
 	const [data, setData] = useState();
@@ -15,10 +16,13 @@ function App() {
 				const { VITE_API_URL, VITE_API_KEY } = import.meta.env;
 				const responses = await Promise.all([
 					fetch(
-						`${VITE_API_URL}games?key=${VITE_API_KEY}&page_size=10&dates=2024-01-01,2024-02-10&ordering=-rating`
+						`${VITE_API_URL}games?key=${VITE_API_KEY}&page_size=10&dates=2024-01-01,2024-02-19&ordering=-rating`
 					),
 					fetch(
 						`${VITE_API_URL}games?key=${VITE_API_KEY}&page_size=1&dates=2024-02-01,2024-02-15&ordering=-added`
+					),
+					fetch(
+						`${VITE_API_URL}games?key=${VITE_API_KEY}&page_size=9&dates=2024-02-20,2024-12-31&ordering=-added`
 					),
 				]);
 				responses.forEach(response => {
@@ -46,14 +50,15 @@ function App() {
 	}
 
 	return (
-		<div className='bg-firmament_blue-950 flex flex-col gap-12'>
+		<div className='bg-firmament_blue-950'>
 			<header className='h-screen'>
 				<Nav />
 				{loading && <CarouselPlaceholder />}
 				{!loading && <Carousel slides={data[0].results} />}
 			</header>
-			<main className='text-atomic_orange-50 px-20'>
+			<main className='text-atomic_orange-50 px-20 flex flex-col gap-14 py-14'>
 				{!loading && <Trending game={data[1].results} />}
+				{!loading && <Upcomming games={data[2].results} />}
 			</main>
 		</div>
 	);
