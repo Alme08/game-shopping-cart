5d157a5d64289474146871b50768ea816173fff3
Alme <josealejandrogilmendez08@gmail.com>
2024-02-21 21:08:44 -0400

    Set placeholder components

---
+++ src/components/Placeholder/TrendingPlaceholder.jsx
@@ -0,0 +1,30 @@
+import Button from '../Button';
+function TrendingPlaceholder() {
+	return (
+		<section>
+			<div className='flex items-center justify-between pb-8'>
+				<h2 className='font-modern text-4xl'>
+					Trending <span className='text-atomic_orange-950'>Now!</span>
+				</h2>
+				<Button text={'View all'} primary={true} />
+			</div>
+			<div className='flex gap-5'>
+				<div className='aspect-video w-full rounded-lg bg-autumn_white-50/30' />
+				<div className='w-full flex flex-col justify-between py-3'>
+					<div className='flex flex-col gap-5'>
+						<span className='h-7 w-40 bg-autumn_white-50/30 rounded-lg' />
+						<span className='h-14 w-60 bg-autumn_white-50/30 rounded-lg' />
+						<span className='h-7 w-36 bg-autumn_white-50/30 rounded-lg' />
+						<span className='h-10 w-24 bg-autumn_white-50/30 rounded-lg' />
+					</div>
+					<div className='flex justify-center gap-10'>
+						<Button text={'Details'} />
+						<Button text={'Buy now'} primary={true} />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default TrendingPlaceholder;
---
+++ src/components/Placeholder/UpcomingPlaceholder.jsx
@@ -0,0 +1,29 @@
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
+
+function UpcomingPlaceholder() {
+	return (
+		<section>
+			<h2 className='font-modern text-4xl pb-8'>
+				Up<span className='text-atomic_orange-950'>coming</span>
+			</h2>
+			<div
+				className='relative flex items-center text-3xl px-20 
+            [&>button]:px-2 [&>button]:from-atomic_orange-700 [&>button]:to-atomic_orange-950 [&>button]:rounded-md [&>button]:absolute [&>button]:z-10'
+			>
+				<button className='bg-gradient-to-r swiper-button-prev -left-10'>
+					<IoIosArrowBack />
+				</button>
+				<div className='flex w-full justify-between'>
+					<div className='p-3 bg-autumn_white-50/30 rounded-xl w-[23rem] h-[28rem]' />
+					<div className='p-3 bg-autumn_white-50/30 rounded-xl w-[23rem] h-[28rem]' />
+					<div className='p-3 bg-autumn_white-50/30 rounded-xl w-[23rem] h-[28rem]' />
+				</div>
+				<button className='bg-gradient-to-l swiper-button-next -right-10'>
+					<IoIosArrowForward />
+				</button>
+			</div>
+		</section>
+	);
+}
+
+export default UpcomingPlaceholder;
