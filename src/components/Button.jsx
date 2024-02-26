1239a4c68134e6c21320934660aeb8dbfd6ebf9d
Alme <josealejandrogilmendez08@gmail.com>
2024-02-26 10:17:50 -0400

    Add px, py and fontSize props and default props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function Button({ text, primary }) {
+function Button({ text, primary, px, py, fontSize }) {
 	return (
 		<>
 			<Link
 				to='#'
-				className={`bg-gradient-to-br px-12 py-2 font-modern rounded-md cursor-pointer text-3xl ${
+				className={`bg-gradient-to-br px-${px} py-${py} font-modern rounded-md cursor-pointer text-${fontSize}xl ${
 					primary
 						? 'from-atomic_orange-700 to-atomic_orange-950'
 						: 'from-firmament_blue-700 to-firmament_blue-950'
@@ -21,10 +21,16 @@ function Button({ text, primary }) {
 Button.propTypes = {
 	text: PropTypes.string,
 	primary: PropTypes.bool,
+	px: PropTypes.number,
+	py: PropTypes.number,
+	fontSize: PropTypes.number,
 };
 
 Button.defaultProps = {
 	text: 'lorem ipsum',
 	primary: false,
+	px: 12,
+	py: 2,
+	fontSize: 3,
 };
 export default Button;
