48c1d7f1ee97ea0a1906cfe628205c0526a27606
Alme <josealejandrogilmendez08@gmail.com>
2024-03-01 13:07:00 -0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,45 +29,46 @@ function Footer() {
 						corrupti maiores rerum, deleniti consequatur unde optio harum vero
 						porro.
 					</p>
-					<div className='[&_span]:text-autumn_white-50 [&_span]:text-2xl [&_span]:p-2 [&_span]:rounded-lg flex justify-center gap-5 pt-5'>
-						<span className='bg-[#FF0000]'>
+					<div className='[&_a]:text-autumn_white-50 [&_a]:text-2xl [&_a]:p-2 [&_a]:rounded-lg [&_a]:cursor-pointer [&_a]:transition-colors flex justify-center gap-5 pt-5'>
+						<a className='bg-[rgb(255,0,0)] hover:bg-[rgb(255,0,0,0.8)]'>
 							<FaYoutube />
-						</span>
-						<span className='bg-[#4267B2]'>
+						</a>
+						<a className='bg-[rgb(66,103,178)] hover:bg-[rgb(66,103,178,0.8)]'>
 							<FaFacebookF />
-						</span>
-						<span className='bg-[#833AB4]'>
+						</a>
+						<a className='bg-[rgb(131,58,180)] hover:bg-[rgb(131,58,180,0.8)]'>
 							<FaInstagram />
-						</span>
-						<span className='bg-[#25D366]'>
+						</a>
+						<a className='bg-[rgb(37,211,102)] hover:bg-[rgb(37,211,102,0.8)]'>
 							<FaWhatsapp />
-						</span>
+						</a>
 					</div>
 				</div>
 				<div
 					className='flex flex-col gap-5 items-center
                             [&>div]:flex [&>div]:items-center [&>div]:gap-4 [&>div]:text-autumn_white-50/70
+							[&>div]:p-3 [&>div]:rounded-xl [&>div]:transition-colors [&>div]:cursor-pointer
                             [&>div>img]:h-12
                             [&_a]:text-autumn_white-50 [&_a]:cursor-pointer'
 				>
 					<h2>
 						trending <span>news</span>
 					</h2>
-					<div>
+					<div className='hover:bg-firmament_blue-900'>
 						<img src='src\assets\news.jpg' alt='news' />
 						<div>
 							<a>Lorem ipsum dolor sit amet.</a>
 							<p>Action, Adventure.</p>
 						</div>
 					</div>
-					<div>
+					<div className='hover:bg-firmament_blue-900'>
 						<img src='src\assets\news.jpg' alt='news' />
 						<div>
 							<a>Lorem ipsum dolor sit amet.</a>
 							<p>Action, Adventure.</p>
 						</div>
 					</div>
-					<div>
+					<div className='hover:bg-firmament_blue-900'>
 						<img src='src\assets\news.jpg' alt='news' />
 						<div>
 							<a>Lorem ipsum dolor sit amet.</a>
@@ -81,28 +82,32 @@ function Footer() {
 					</h2>
 					<div
 						className='grid grid-cols-2 gap-x-4 gap-y-2
-                                [&>div]:bg-firmament_blue-900 [&>div]:p-3 [&>div]:rounded-xl [&>div]:flex [&>div]:gap-3 [&>div]:justify-center [&>div]:items-center [&>div>p]:text-lg [&>div>p]:flex [&>div>p]:flex-col [&>div>p]:items-center [&>div>p]:text-autumn_white-50/70 [&_span]:text-autumn_white-50'
+
+                                [&>div]:bg-firmament_blue-900 [&>div]:p-3 [&>div]:rounded-xl [&>div]:flex [&>div]:gap-3 [&>div]:justify-between [&>div]:items-center [&>div]:transition-colors
+								 
+								[&>div>p]:text-lg [&>div>p]:flex [&>div>p]:flex-col [&>div>p]:items-center [&>div>p]:text-autumn_white-50/70 [&>div>p]:m-auto 
+								[&_span]:text-autumn_white-50'
 					>
-						<div>
-							<FaGooglePlay className='text-4xl' />
+						<div className='hover:bg-firmament_blue-950'>
+							<FaGooglePlay className='text-4xl m-auto' />
 							<p>
 								Buy now via <span>Google Play</span>
 							</p>
 						</div>
-						<div>
-							<FaApple className='text-4xl' />
+						<div className='hover:bg-firmament_blue-950'>
+							<FaApple className='text-4xl m-auto' />
 							<p>
 								Buy now via <span>Apple Store</span>
 							</p>
 						</div>
-						<div>
-							<FaSteam className='text-4xl' />
+						<div className='hover:bg-firmament_blue-950'>
+							<FaSteam className='text-4xl m-auto' />
 							<p>
 								Buy now via <span>Steam</span>
 							</p>
 						</div>
-						<div>
-							<FaWindows className='text-4xl' />
+						<div className='hover:bg-firmament_blue-950'>
+							<FaWindows className='text-4xl m-auto' />
 							<p>
 								Download via <span>WinStore</span>
 							</p>
@@ -117,10 +122,15 @@ function Footer() {
 				>
 					<FaGithub className='text-xl' /> Alme08
 				</a>
-				<a className='flex gap-5 w-max' href='https://rawg.io/apidocs'>
+				<a className='flex gap-3 w-max' href='https://rawg.io/apidocs'>
 					<img className='h-5' src='src\assets\rawgfavicon.ico' /> rawg api
 				</a>
-				<p>Design inspired by: rayhanulislam.com</p>
+				<p>
+					Design inspired by:{' '}
+					<a href='https://rayhanulislam.com' className='underline'>
+						rayhanulislam.com
+					</a>
+				</p>
 			</div>
 		</>
 	);
