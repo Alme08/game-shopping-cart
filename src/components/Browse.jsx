0c2331e194f03998b38a39e62080bb475ea8b947
Alme <josealejandrogilmendez08@gmail.com>
2024-03-12 15:24:50 -0400

    Refactor the sidebar to use an array of objects to render the links.
    This will allow us to add more links to the sidebar without having to
    add more code.

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -15,8 +15,33 @@ import { FaChartSimple, FaCrown, FaGun } from 'react-icons/fa6';
 import { RiBoxingFill, RiSwordFill } from 'react-icons/ri';
 import Nav from './Nav';
 
+const links = [
+	'New Releases',
+	[
+		{ svg: FaStar, text: 'Last 30 days' },
+		{ svg: FaFire, text: 'This week' },
+		{ svg: FaAngleDoubleRight, text: 'Next week' },
+	],
+	'Top',
+	[
+		{ svg: FaTrophy, text: 'Best of the year' },
+		{ svg: FaChartSimple, text: `Popular in ${getYear(new Date())}` },
+		{ svg: FaCrown, text: 'All time top' },
+	],
+	'Genres',
+	[
+		{ svg: RiBoxingFill, text: 'Action' },
+		{ svg: FaChessKnight, text: 'Strategy' },
+		{ svg: RiSwordFill, text: 'RPG' },
+		{ svg: FaGun, text: 'shooter' },
+		{ svg: FaMountain, text: 'Adventure' },
+		{ svg: FaPuzzlePiece, text: 'Puzzle' },
+		{ svg: FaFlagCheckered, text: 'Racing' },
+		{ svg: FaFootballBall, text: 'Sports' },
+	],
+];
+
 function Browse() {
-	const year = getYear(new Date());
 	return (
 		<div className='flex bg-firmament_blue-950 h-screen'>
 			<Nav bgActive={true} />
@@ -25,75 +50,30 @@ function Browse() {
 				className='sticky overflow-y-scroll flex flex-col gap-10 bg-firmament_blue-950 text-autumn_white-50 w-1/5 p-10 pt-20'
 			>
 				<section
-					className=' flex flex-col gap-8 [&>div]:flex [&>div]:flex-col [&_h2]:text-3xl [&_h2]:font-bold [&_h2]:pb-2
-        [&_button]:flex [&_button]:p-4 [&_button]:gap-3 [&_button]:items-center [&_button]:text-xl
-        [&_button:hover]:bg-autumn_white-50/10  [&_button]:rounded-xl
-        [&_button>p]:text-lg'
+					className=' flex flex-col gap-8 
+								[&_div]:flex [&_div]:flex-col 
+								[&_h2]:text-3xl [&_h2]:font-bold [&_h2]:py-2
+								[&_button]:flex [&_button]:p-4 [&_button]:gap-3 [&_button]:items-center [&_button]:text-xl
+								[&_button:hover]:bg-autumn_white-50/10  [&_button]:rounded-xl
+								[&_button>p]:text-lg'
 				>
 					<div>
-						<h2>New Releases</h2>
-						<button>
-							<FaStar />
-							<p>Last 30 days</p>
-						</button>
-						<button>
-							<FaFire />
-							<p>This week</p>
-						</button>
-						<button>
-							<FaAngleDoubleRight />
-							<p>Next week</p>
-						</button>
-					</div>
-					<div>
-						<h2>Top</h2>
-						<button>
-							<FaTrophy />
-							<p>Best of the year</p>
-						</button>
-						<button>
-							<FaChartSimple />
-							<p>Popular in {year}</p>
-						</button>
-						<button>
-							<FaCrown />
-							<p>All time top</p>
-						</button>
-					</div>
-					<div>
-						<h2>Genres</h2>
-						<button>
-							<RiBoxingFill />
-							<p>Action</p>
-						</button>
-						<button>
-							<FaChessKnight />
-							<p>Strategy</p>
-						</button>
-						<button>
-							<RiSwordFill />
-							<p>RPG</p>
-						</button>
-						<button>
-							<FaGun />
-							<p>shooter</p>
-						</button>
-						<button>
-							<FaMountain />
-							<p>Adventure</p>
-						</button>
-						<button>
-							<FaPuzzlePiece />
-							<p>Puzzle</p>
-						</button>
-						<button>
-							<FaFlagCheckered />
-							<p>Racing</p>
-						</button>
-						<button>
-							<FaFootballBall />
-							<p>Sports</p>
-						</button>
+						{links.map((link, index) => (
+							<div key={index}>
+								{Array.isArray(link) ? (
+									<>
+										{link.map((subLink, index) => (
+											<button key={index}>
+												<subLink.svg />
+												<p>{subLink.text}</p>
+											</button>
+										))}
+									</>
+								) : (
+									<h2>{link}</h2>
+								)}
+							</div>
+						))}
 					</div>
 				</section>
 			</aside>
