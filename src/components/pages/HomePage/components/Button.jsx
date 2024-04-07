f79c920e54f2bf730c4b43fa1e788a2ff3ce3736
Alme <josealejandrogilmendez08@gmail.com>
2024-04-07 14:38:50 -0400

    Add 'to' attribute, proptype and defaultprop to Button component

--- src/components/pages/HomePage/components/Button.jsx
+++ src/components/pages/HomePage/components/Button.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function Button({ text, primary, fontSize }) {
+function Button({ text, primary, fontSize, to }) {
 	return (
 		<>
 			<Link
-				to='#'
+				to={to}
 				className={`bg-gradient-to-br py-2 px-10 font-modern rounded-md cursor-pointer text-${fontSize}xl ${
 					primary
 						? 'from-atomic_orange-700 to-atomic_orange-950'
@@ -22,11 +22,13 @@ Button.propTypes = {
 	text: PropTypes.string,
 	primary: PropTypes.bool,
 	fontSize: PropTypes.number,
+	to: PropTypes.string,
 };
 
 Button.defaultProps = {
 	text: 'lorem ipsum',
 	primary: false,
 	fontSize: 3,
+	to: '/',
 };
 export default Button;
